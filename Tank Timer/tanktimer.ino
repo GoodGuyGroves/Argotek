03c463822b635835aa9b2220a9e1c8b7db816754
Russell Groves <russellgroves@gmail.com>
2015-01-26 18:42:32 +0200

    Edited for 4 tanks

--- Tank Timer/tanktimer.ino
+++ Tank Timer/tanktimer.ino
@@ -1,34 +1,79 @@
-const int timerBuzzer = 12;
+const int timerBuzzer = 19;
 
-const int nickelBtnOn = 2;
-const int nickelBtnOff = 3;
-const int nickelLEDBusy = 4;
-const int nickelLEDDone = 5;
-const unsigned long nickelTimerDuration = 1080000; // Duration in milliseconds - 18 minutes
+// Alkaline
+const int alkalineBtnOn = 3;
+const int alkalineBtnOff = 4;
+const int alkalineLEDBusy = 5;
+const int alkalineLEDDone = 6;
+const unsigned long alkalineTimerDuration = 180000; // Duration in milliseconds - 3 minutes
 
-const int chromeBtnOn = 6;
-const int chromeBtnOff = 7;
-const int chromeLEDBusy = 8;
-const int chromeLEDDone = 9;
-const unsigned long chromeTimerDuration = 150000; // Duration in milliseconds - 2.5 minutes
+// Nickel - One
+const int nickel1BtnOn = 7;
+const int nickel1BtnOff = 8;
+const int nickel1LEDBusy = 9;
+const int nickel1LEDDone = 10;
+const unsigned long nickel1TimerDuration = 900000; // Duration in milliseconds - 15 minutes
 
-boolean nickelTimerState = false;
-unsigned long nickelTimerEpoch = 0;
-int nickelBtnOnState = 0;
-int nickelBtnOffState = 0;
+// Nickel - Two
+const int nickel2BtnOn = 11;
+const int nickel2BtnOff = 12;
+const int nickel2LEDBusy = 13;
+const int nickel2LEDDone = 14;
+const unsigned long nickel2TimerDuration = 900000; // Duration in milliseconds - 15 minutes
 
+// Chrome
+const int chromeBtnOn = 15;
+const int chromeBtnOff = 16;
+const int chromeLEDBusy = 17;
+const int chromeLEDDone = 18;
+const unsigned long chromeTimerDuration = 120000; // Duration in milliseconds - 2 minutes
+
+// Alkaline
+boolean alkalineTimerState = false;
+unsigned long alkalineTimerEpoch = 0;
+int alkalineBtnOnState = 0;
+int alkalineBtnOffState = 0;
+
+// Nickel - One
+boolean nickel1TimerState = false;
+unsigned long nickel1TimerEpoch = 0;
+int nickel1BtnOnState = 0;
+int nickel1BtnOffState = 0;
+
+// Nickel - Two
+boolean nickel2TimerState = false;
+unsigned long nickel2TimerEpoch = 0;
+int nickel2BtnOnState = 0;
+int nickel2BtnOffState = 0;
+
+// Chrome
 boolean chromeTimerState = false;
 unsigned long chromeTimerEpoch = 0;
 int chromeBtnOnState = 0;
 int chromeBtnOffState = 0;
 
 void setup(){
-  Serial.begin(1200);
-  pinMode(nickelBtnOn, INPUT);
-  pinMode(nickelBtnOff, INPUT);
-  pinMode(nickelLEDBusy, OUTPUT);
-  pinMode(nickelLEDDone, OUTPUT);
+  Serial.begin(115200);//1200);
 
+  // Alkaline
+  pinMode(alkalineBtnOn, INPUT);
+  pinMode(alkalineBtnOff, INPUT);
+  pinMode(alkalineLEDBusy, OUTPUT);
+  pinMode(alkalineLEDDone, OUTPUT);
+
+  // Nickel - One
+  pinMode(nickel1BtnOn, INPUT);
+  pinMode(nickel1BtnOff, INPUT);
+  pinMode(nickel1LEDBusy, OUTPUT);
+  pinMode(nickel1LEDDone, OUTPUT);
+
+  // Nickel - Two
+  pinMode(nickel2BtnOn, INPUT);
+  pinMode(nickel2BtnOff, INPUT);
+  pinMode(nickel2LEDBusy, OUTPUT);
+  pinMode(nickel2LEDDone, OUTPUT);
+
+  // Chrome
   pinMode(chromeBtnOn, INPUT);
   pinMode(chromeBtnOff, INPUT);
   pinMode(chromeLEDBusy, OUTPUT);
@@ -36,32 +81,89 @@ void setup(){
 }
 
 void  loop() {
-  nickelBtnOnState = digitalRead(nickelBtnOn);
-  nickelBtnOffState = digitalRead(nickelBtnOff);
-  if (nickelBtnOnState == 0) {
-    nickelTimerState = true;
+
+  // Alkaline
+  alkalineBtnOnState = digitalRead(alkalineBtnOn);
+  alkalineBtnOffState = digitalRead(alkalineBtnOff);
+
+  if (alkalineBtnOnState == 0) {
+    alkalineTimerState = true;
   }
-  if (nickelBtnOffState == 0) {
-    nickelTimerState = false;
-    nickelTimerEpoch = 0;
-    digitalWrite(nickelLEDBusy, LOW);
-    digitalWrite(nickelLEDDone, LOW);
+  if (alkalineBtnOffState == 0) {
+    alkalineTimerState = false;
+    alkalineTimerEpoch = 0;
+    digitalWrite(alkalineLEDBusy, LOW);
+    digitalWrite(alkalineLEDDone, LOW);
     noTone(timerBuzzer);
   }
-  if (nickelTimerState){
-    if (nickelTimerEpoch == 0){
-      nickelTimerEpoch = millis();
-      digitalWrite(nickelLEDBusy, HIGH);
+  if (alkalineTimerState){
+    if (alkalineTimerEpoch == 0){
+      alkalineTimerEpoch = millis();
+      digitalWrite(alkalineLEDBusy, HIGH);
     }
-    if (millis() - nickelTimerEpoch >= nickelTimerDuration){
-      digitalWrite(nickelLEDBusy, LOW);
-      digitalWrite(nickelLEDDone, HIGH);
+    if (millis() - alkalineTimerEpoch >= alkalineTimerDuration){
+      digitalWrite(alkalineLEDBusy, LOW);
+      digitalWrite(alkalineLEDDone, HIGH);
       tone(timerBuzzer, 1000);
     }
   }
 
+  // Nickel - One  
+  nickel1BtnOnState = digitalRead(nickel1BtnOn);
+  nickel1BtnOffState = digitalRead(nickel1BtnOff);
+
+  if (nickel1BtnOnState == 0) {
+    nickel1TimerState = true;
+  }
+  if (nickel1BtnOffState == 0) {
+    nickel1TimerState = false;
+    nickel1TimerEpoch = 0;
+    digitalWrite(nickel1LEDBusy, LOW);
+    digitalWrite(nickel1LEDDone, LOW);
+    noTone(timerBuzzer);
+  }
+  if (nickel1TimerState){
+    if (nickel1TimerEpoch == 0){
+      nickel1TimerEpoch = millis();
+      digitalWrite(nickel1LEDBusy, HIGH);
+    }
+    if (millis() - nickel1TimerEpoch >= nickel1TimerDuration){
+      digitalWrite(nickel1LEDBusy, LOW);
+      digitalWrite(nickel1LEDDone, HIGH);
+      tone(timerBuzzer, 1000);
+    }
+  }
+
+  // Nickel - Two
+  nickel2BtnOnState = digitalRead(nickel2BtnOn);
+  nickel2BtnOffState = digitalRead(nickel2BtnOff);
+
+  if (nickel2BtnOnState == 0) {
+    nickel2TimerState = true;
+  }
+  if (nickel2BtnOffState == 0) {
+    nickel2TimerState = false;
+    nickel2TimerEpoch = 0;
+    digitalWrite(nickel2LEDBusy, LOW);
+    digitalWrite(nickel2LEDDone, LOW);
+    noTone(timerBuzzer);
+  }
+  if (nickel2TimerState){
+    if (nickel2TimerEpoch == 0){
+      nickel2TimerEpoch = millis();
+      digitalWrite(nickel2LEDBusy, HIGH);
+    }
+    if (millis() - nickel2TimerEpoch >= nickel2TimerDuration){
+      digitalWrite(nickel2LEDBusy, LOW);
+      digitalWrite(nickel2LEDDone, HIGH);
+      tone(timerBuzzer, 1000);
+    }
+  }
+
+  // Chrome
   chromeBtnOnState = digitalRead(chromeBtnOn);
   chromeBtnOffState = digitalRead(chromeBtnOff);
+
   if (chromeBtnOnState == 0) {
     chromeTimerState = true;
   }
@@ -83,10 +185,17 @@ void  loop() {
       tone(timerBuzzer, 1000);
     }
   }
-  //  Serial.print("Nickel Timer State: ");
-  //  Serial.println(nickelTimerState);
-  //  Serial.print("Nickel Timer Epoch: ");
-  //  Serial.println(nickelTimerEpoch);
-  //  Serial.print("Current Time: ");
-  //  Serial.println(millis());
+
+  // Debugging
+  //  if (nickel1TimerState) {
+  //    Serial.print("Milliseconds: ");
+  //    Serial.println(millis());
+  //    Serial.print("Timer Epoch: ");
+  //    Serial.println(nickel1TimerEpoch);
+  //    Serial.print("Time elapsed: ");
+  //    Serial.println(millis() - nickel1TimerEpoch);
+  //    Serial.print("Timer Duration: ");
+  //    Serial.println(nickel1TimerDuration);
+  //  }
+
 }
